3c196c81f2fdf763161872330d7ae5b689af0f3b
Mengqiu (Roger) Chen <rogermchen.ubc@gmail.com>
2023-08-15 06:37:59 -0600

    Enable mdx support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
+const withMDX = require('@next/mdx')({
+    extension: /\.mdx?$/,
+    options: {
+        remarkPlugins: [],
+        rehypePlugins: [],
+      },
+
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+    reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig)
