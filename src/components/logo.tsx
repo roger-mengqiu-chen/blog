4f1e4c245b50b27c282abffd2328793672e4c31c
Roger Chen <rogermchen.ubc@gmail.com>
2024-10-26 19:15:29 -0600

    update

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,12 +1,15 @@
 import Avatar from '@mui/material/Avatar';
 import Link from 'next/link';
+import Box from '@mui/material/Box';
 
 
 const Logo = () => {
 	return (
-		<Link href="/">
-			<Avatar src="/icon.jpeg" alt="logo" />
-		</Link>
+		<Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Link href="/" passHref>
+        <Avatar src="/icon.jpeg" alt="logo" />
+      </Link>
+    </Box>
 	);
 }
 
