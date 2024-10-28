50b7cf891a945a54d14fafb8ff6d161455e2bb56
Roger Chen <rogermchen.ubc@gmail.com>
2024-10-27 20:04:22 -0600

    make post clickable

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,15 +1,17 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Link } from "@mui/material";
 import { PostMetadata } from "@/utils/getPostMetadata";
 import React from "react";
 
 export const Post: React.FC<PostMetadata> = (post: PostMetadata) =>{
 	
 	return (
-		<Box>
-			<Typography variant="h4">{post.title}</Typography>
-			<Typography variant="body1">{post.description}</Typography>
-			<Typography variant="body2">{post.time}</Typography>
-		</Box>
+		<Link href={`/blog/${post.slug}`}>
+			<Box>
+				<Typography variant="h4">{post.title}</Typography>
+				<Typography variant="body1">{post.description}</Typography>
+				<Typography variant="body2">{post.time}</Typography>
+			</Box>
+		</Link>
 	)
 }
 
