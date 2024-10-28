a16106967337771e8c2d07a412f6f1f7a333c783
Roger Chen <rogermchen.ubc@gmail.com>
2024-10-27 20:04:32 -0600

    render post

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,9 +1,49 @@
-export const BlogPag = () => {
+import Markdown from "markdown-to-jsx";
+import { getPostMetadata } from "@/utils/getPostMetadata";
+import React from "react";
+import * as fs from 'fs';
+import matter from "gray-matter";
 
+
+const getPostContent = (slug: string) => {
+  const file = `src/blogs/${slug}.md`;
+  const content = fs.readFileSync(file, "utf-8");
+
+  const matterResult = matter(content);
+  return matterResult;
+}
+
+export const generateStaticParams = async () => {
+  const posts = getPostMetadata("src/blogs");
+  return posts.map((post) => ({ slug: post.slug }));
+}
+
+export const generateMetadata = async ({ params }: { params: { slug: string } }) => {
+  const { slug } = params;
+  const post = getPostContent(slug);
+
+  return {
+    title: `Roger Chen's Blog ${slug}`,
+    description: post.data.description,
+  }
+}
+
+interface BlogPageProps {
+  params: {
+    slug: string;
+  };
+}
+
+const BlogPag: React.FC<BlogPageProps> = ({ params }) => {
+  const { slug } = params;
+  const post = getPostContent(slug);
+  console.log(post);
   return (
-    <div>
-      <h1>Blog Page</h1>
-    </div>
+    <main>
+      <article>
+        <Markdown>{post.content}</Markdown>
+      </article>
+    </main>
   );
 }
 
