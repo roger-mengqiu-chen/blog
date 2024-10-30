9510a32363622a3991e75e68adbffd5749bf2963
Roger Chen <rogermchen.ubc@gmail.com>
2024-10-30 13:42:28 -0600

    App bar in home page is redundant

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 
-import ResponsiveAppBar from "@/components/header/header";
 import Box from '@mui/material/Box';
 import Main from '@/components/home/home';
 
@@ -8,7 +7,6 @@ export default function Home() {
   return (
     <>
       <Box sx={{ display: 'flex' }}>
-        <ResponsiveAppBar />
         <Main />
       </Box>    
     </>
