98e62b77fa995a9bbfc3ec062ddc0bc6d6e0dd56
Roger Chen <rogermchen.ubc@gmail.com>
2024-10-30 16:46:24 -0600

    styles for post cards and added another dummy post

---
+++ src/blogs/second-blog.md
@@ -0,0 +1,13 @@
+---
+title: "Second Blog"
+description: "This is my second blog"
+time: "2024-10-26 12:00:00"
+---
+
+# First Blog
+
+This is my second blog. I am so excited to start my blogging journey.
+
+```python
+print('hello again')
+```
--- src/components/home/post.tsx
+++ src/components/home/post.tsx
@@ -1,17 +1,27 @@
-import { Box, Typography, Link } from "@mui/material";
+import { Paper, Box, Typography, Link } from "@mui/material";
 import { PostMetadata } from "@/utils/getPostMetadata";
 import React from "react";
+import styles from '@/components/home/post.module.css';
+import Image from 'next/image';
 
-export const Post: React.FC<PostMetadata> = (post: PostMetadata) =>{
+export const Post: React.FC<PostMetadata> = (post: PostMetadata) => {
+	const defaultThumbnail = "/images/default-thumbnail.jpg";	
 	
 	return (
-		<Link href={`/blog/${post.slug}`}>
-			<Box>
-				<Typography variant="h4">{post.title}</Typography>
-				<Typography variant="body1">{post.description}</Typography>
-				<Typography variant="body2">{post.time}</Typography>
-			</Box>
-		</Link>
+		<Paper elevation={3} className={styles.card}>
+			<Link href={`/blog/${post.slug}`} className={styles.post_link}>
+				<Box className={styles.card_content}>
+					<Box className={styles.thumbnail_container}>
+						<Image src={defaultThumbnail} alt={post.title} width={0} height={0} className={ styles.thumbnail }/>
+					</Box>
+					<Box>
+						<Typography className={styles.title}>{post.title}</Typography>
+						<Typography variant="body2">{post.time}</Typography>
+						<Typography variant="body1">{post.description}</Typography>
+					</Box>
+				</Box>
+			</Link>
+		</Paper>
 	)
 }
 
