ce6f283f7d764a030146c4fa8e41b22d02f5cf52
Roger Chen <rogermchen.ubc@gmail.com>
2024-10-29 21:20:03 -0600

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import Header from "@/components/header/header";
 import { Toolbar, Box } from "@mui/material";
 
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,7 +4,6 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
 import List from '@mui/material/List';
@@ -15,7 +14,6 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import Logo from '@/components/header/logo';
-import { Typography } from '@mui/material';
 import Brand from './brand';
 
 
