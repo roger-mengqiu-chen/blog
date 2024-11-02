e0ab4b4371eb1f9e4b338e2061a11dcfe112c544
Roger Chen <rogermchen.ubc@gmail.com>
2024-11-01 22:10:55 -0600

    update image style

--- src/components/home/custom_image.tsx
+++ src/components/home/custom_image.tsx
@@ -9,7 +9,7 @@ interface CustomImageProps {
 const CustomImage = ({ src, alt }: CustomImageProps) => {
   src = "../images/" + src;
   return (
-    <Image src={src} alt={alt} width={600} height={400} className={styles.post_image}/>
+    <Image src={src} alt={alt} width={0} height={0} className={styles.post_image}/>
   )
 }
 
