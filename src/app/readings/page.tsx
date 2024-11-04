a34bee8dd3ebc73e842344cb4c80df26d5262a0b
Roger Chen <rogermchen.ubc@gmail.com>
2024-11-03 18:09:52 -0700

    update

--- src/app/readings/page.tsx
+++ src/app/readings/page.tsx
@@ -1,7 +1,36 @@
+import Markdown from "markdown-to-jsx";
+import { Box } from "@mui/material";
+import { getPostContent } from "@/utils/post_utils";
+import React from "react";
+import CustomImage from "@/components/home/custom_image";
+
+
+export const generateStaticParams = async () => {
+	return [{slug: "readings"}]
+}
+
+export const generateMetadata = async () => {
+  return {
+    title: `Roger Chen's Readings`,
+    description: "Books read by Roger Chen",
+  }
+}
+
 const ReadingPage = () => {
-    return (
-        <></>
-    )
+  const post = getPostContent("readings", "readings");
+
+  return (
+    <Box component="article">
+      <Markdown
+        options={{
+        overrides: {
+          img: {
+            component: CustomImage,
+          },
+        },
+      }}>{post.content}</Markdown>
+    </Box>
+  );
 }
 
 export default ReadingPage;
---
+++ src/data/readings/readings.md
@@ -0,0 +1,9 @@
+# My readings
+
+|  |  |
+|-------|--------|
+| **Clean Coder, The: A Code of Conduct for Professional Programmers** | _Robert C. Martin_ |
+| **Using Asyncio in Python** | _Caleb Hattingh_ |
+| **C Primer Plus, Sixth Edition** | _Stephen Prata_ |
+| **C++ Primer Plus, Fifth Edition** | _Stephen Prata_ |
+| **The Innovators’ Mindset: Empower Learning, Unleash Talent, and Lead a Culture of Creativity** | _George Couros_ |
