8200763b288dddad5f937c9e158a64d29e4f2944
Roger Chen <rogermchen.ubc@gmail.com>
2025-01-08 22:38:46 -0700

    remove duplicate tags

--- src/components/home/tag_cloud.tsx
+++ src/components/home/tag_cloud.tsx
@@ -8,9 +8,10 @@ const TagCloud = () => {
   const project_posts = getPostMetadata("src/data/project");
   const all_posts = blog_posts.concat(project_posts);
   const tags = all_posts.map(post => post.tags).flat();
+  const uniqueTags = Array.from(new Set(tags));
   return (
     <Box>
-      {tags.map((tag, index) => {
+      {uniqueTags.map((tag, index) => {
         return (
           <Chip key={index} className={styles.tag} label={tag} size="medium"/>
         );
