0c73b501cb07df1e154fc30b30821b369414d27e
Roger Chen <rogermchen.ubc@gmail.com>
2026-08-23 13:18:53 -0600

    update

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import createMDX from '@next/mdx';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: '/blog',
     pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
     output: 'export',
     images: { unoptimized: true }
