a97dfde21b1eea470dc1f7cd5d27b31c8a71efa9
Roger Chen <rogermchen.ubc@gmail.com>
2026-08-23 15:38:17 -0600

    update

--- src/components/header/logo.tsx
+++ src/components/header/logo.tsx
@@ -2,13 +2,15 @@ import Link from 'next/link';
 import Box from '@mui/material/Box';
 import Image from 'next/image';
 import styles from '@/components/header/logo.module.css';
+import {normalizePublicAsset} from "@/utils/path_utils";
 
 
 const Logo = () => {
+    const logoSrc = normalizePublicAsset("/icon.jpeg"); // Path to your logo image
 	return (
 		<Box className={styles.logoBox}>
       <Link href="/" passHref>
-        <Image src="/icon.jpeg" alt="logo" width={50} height={50} style={{borderRadius: '50%'}}/>
+        <Image src={logoSrc} alt="logo" width={50} height={50} style={{borderRadius: '50%'}}/>
       </Link>
     </Box>
 	);
--- src/utils/path_utils.ts
+++ src/utils/path_utils.ts
@@ -2,6 +2,6 @@ export const basePath =
     process.env.NEXT_PUBLIC_BASE_PATH ||
     (process.env.NODE_ENV === "production" ? "/blog" : "/");
 
-export function normalizePublicAsset(src: string) {
+export const normalizePublicAsset = (src: string) => {
     return `${basePath}${src.startsWith("/") ? src : `/${src}`}`;
 }
