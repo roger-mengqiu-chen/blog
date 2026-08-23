792c87177a493726e224c70a5629a5abe5a4f794
Roger Chen <rogermchen.ubc@gmail.com>
2026-08-23 14:15:04 -0600

    distinguish local and prod env

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,8 @@ import createMDX from '@next/mdx';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/blog',
+    basePath: process.env.NODE_ENV === "production" ? "/blog" : "",
+    trailingSlash: true,
     pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
     output: 'export',
     images: { unoptimized: true }
