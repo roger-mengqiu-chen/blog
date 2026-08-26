fe3b53cadd31239f8ee454499b904a0082deb9f6
Roger Chen <rogermchen.ubc@gmail.com>
2026-08-25 18:37:11 -0600

    reformat

--- src/components/header/logo.tsx
+++ src/components/header/logo.tsx
@@ -7,13 +7,13 @@ import {normalizePublicAsset} from "@/utils/path_utils";
 
 const Logo = () => {
     const logoSrc = normalizePublicAsset("/icon.jpeg"); // Path to your logo image
-	return (
-		<Box className={styles.logoBox}>
-      <Link href="/" passHref>
-        <Image src={logoSrc} alt="logo" width={50} height={50} style={{borderRadius: '50%'}}/>
-      </Link>
-    </Box>
-	);
+    return (
+        <Box className={styles.logoBox}>
+            <Link href="/" passHref>
+                <Image src={logoSrc} alt="logo" width={50} height={50} style={{borderRadius: '50%'}}/>
+            </Link>
+        </Box>
+    );
 }
 
 export default Logo;
