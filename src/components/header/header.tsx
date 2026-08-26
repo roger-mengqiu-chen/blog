884288c01dd29ffcb46c820773b0f0cc4e441049
Roger Chen <rogermchen.ubc@gmail.com>
2026-08-25 18:38:19 -0600

    reformat

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,7 +2,19 @@
 
 import * as React from 'react';
 import Link from 'next/link';
-import { AppBar, Box, CssBaseline, Drawer, IconButton, List, ListItem, ListItemButton, ListItemText, Toolbar, Button } from '@mui/material';
+import {
+  AppBar,
+  Box,
+  CssBaseline,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Button
+} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import HomeIcon from '@mui/icons-material/Home';
 import ConstructionIcon from '@mui/icons-material/Construction';
@@ -11,9 +23,9 @@ import Brand from './brand';
 
 
 const navItems = [
-  {icon: <HomeIcon />, item: 'Home', link: '/'},
-  {icon: <ConstructionIcon />, item: 'Projects', link: '/project'},
-  {icon: <MenuBookIcon />, item: 'Readings', link: '/readings'}
+  {icon: <HomeIcon/>, item: 'Home', link: '/'},
+  {icon: <ConstructionIcon/>, item: 'Projects', link: '/project'},
+  {icon: <MenuBookIcon/>, item: 'Readings', link: '/readings'}
 ];
 
 export const ResponsiveAppBar = () => {
@@ -25,15 +37,15 @@ export const ResponsiveAppBar = () => {
   };
 
   const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center', p: 3 }}>
-      <Toolbar />
+    <Box onClick={handleDrawerToggle} sx={{textAlign: 'center', p: 3}}>
+      <Toolbar/>
       <List>
-        {navItems.map(({ icon, item, link }) => (
-          <ListItem key={ item } disablePadding>
-            <Link href={ link }>
-              <ListItemButton sx={{ textAlign: 'center' }}>
-                {icon && <Box sx={{ mr: 1 }}>{icon}</Box>}
-                <ListItemText primary={item} sx={{fontSize: "1.2rem", fontWeight: "500" }}/>
+        {navItems.map(({icon, item, link}) => (
+          <ListItem key={item} disablePadding>
+            <Link href={link}>
+              <ListItemButton sx={{textAlign: 'center'}}>
+                {icon && <Box sx={{mr: 1}}>{icon}</Box>}
+                <ListItemText primary={item} sx={{fontSize: "1.2rem", fontWeight: "500"}}/>
               </ListItemButton>
             </Link>
           </ListItem>
@@ -44,27 +56,27 @@ export const ResponsiveAppBar = () => {
 
   return (
     <>
-      <CssBaseline />
-      <AppBar component="nav" position="fixed" sx= {{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+      <CssBaseline/>
+      <AppBar component="nav" position="fixed" sx={{zIndex: (theme) => theme.zIndex.drawer + 1}}>
         <Toolbar>
-          <Box sx={{ display: 'flex', justifyContent: 'space-between', flexGrow: 1, margin: 0 }}>
-            <Box sx={{ display: 'flex' }}>
+          <Box sx={{display: 'flex', justifyContent: 'space-between', flexGrow: 1, margin: 0}}>
+            <Box sx={{display: 'flex'}}>
               <IconButton
                 color="inherit"
                 aria-label="open drawer"
                 edge="start"
-                onClick={ handleDrawerToggle }
-                sx={{ mr: 2, display: { sm: 'none' } }}
+                onClick={handleDrawerToggle}
+                sx={{mr: 2, display: {sm: 'none'}}}
               >
-                <MenuIcon />
+                <MenuIcon/>
               </IconButton>
 
-              <Brand />
+              <Brand/>
             </Box>
-            <Box sx={{ display: { xs: 'none', sm: 'flex' }, alignItems: 'center' }}>
+            <Box sx={{display: {xs: 'none', sm: 'flex'}, alignItems: 'center'}}>
               {navItems.map(({item, link}) => (
                 <Link key={item} href={link} passHref>
-                  <Button key={item} sx={{ color: '#fff' }}>
+                  <Button key={item} sx={{color: '#fff'}}>
                     {item}
                   </Button>
                 </Link>
@@ -79,11 +91,11 @@ export const ResponsiveAppBar = () => {
           open={mobileOpen}
           onClose={handleDrawerToggle}
           ModalProps={{
-            keepMounted: true, 
+            keepMounted: true,
           }}
           sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: 240 },
+            display: {xs: 'block', sm: 'none'},
+            '& .MuiDrawer-paper': {boxSizing: 'border-box', width: 240},
           }}
         >
           {drawer}
