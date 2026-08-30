e9f6a23008c23af4b5584c3db03c2c6cba34cf68
Roger Chen <rogermchen.ubc@gmail.com>
2026-08-29 18:12:08 -0600

    reformat

--- src/app/project/[slug]/page.tsx
+++ src/app/project/[slug]/page.tsx
@@ -1,27 +1,16 @@
-import { getPostMetadata, getPostContent, BlogPageProps, sortPostsByTime } from "@/utils/post_utils";
+import {getPostMetadata, BlogPageProps, sortPostsByTime} from "@/utils/post_utils";
 import React from "react";
-import { PostMain } from "@/components/home/home";
+import {PostMain} from "@/components/home/home";
 
 
 export const generateStaticParams = async () => {
   const posts = getPostMetadata("src/data/project");
   sortPostsByTime(posts);
-  return posts.map((post) => ({ slug: post.slug }));
+  return posts.map((post) => ({slug: post.slug}));
 }
 
-export const generateMetadata = async ({ params }: { params: { slug: string } }) => {
-  const { slug } = params;
-  const posts = getPostMetadata("src/data/project");
-  const postMeta = posts.find((p: { slug: string; }) => p.slug === slug);
-
-  return {
-    title: `Roger Chen's Project | ${slug}`,
-    description: postMeta?.description ?? '',
-  };
-}
-
-const ProjectPage: React.FC<BlogPageProps> = async ({ params }) => {
-  const { slug } = await params as { slug: string };
+const ProjectPage: React.FC<BlogPageProps> = async ({params}) => {
+  const {slug} = await params as { slug: string };
 
   return (
     <PostMain slug={slug} category="project"/>
